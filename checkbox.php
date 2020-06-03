e105f58fefa0c8307524dde0779defb068cfd451
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-03 10:36:15 +0200

    including head.html

--- checkbox.php
+++ checkbox.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("head.html"); ?>
     <title>Séléction de tables de multiplication</title>
 </head>
 <body>
