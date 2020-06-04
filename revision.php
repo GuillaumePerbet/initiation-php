84d29fb4c4b240ba8ab57827f4b9a3409f067ea5
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-04 14:56:10 +0200

    adding select icon

--- revision.php
+++ revision.php
@@ -47,7 +47,7 @@
                 ?>
                 <!-- select table to practice -->
                 <form action="" method="post" class="flex column center">
-                    <div>
+                    <div class="flex center">
                         <label for="number">Choisir la table à réviser :</label>
                         <select name="number" id="number">
                             <?php
@@ -59,6 +59,10 @@
                             }
                             ?>
                         </select>
+
+                        <div class="select-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M7 10l5 5 5-5H7z"/></svg>
+                        </div>
                     </div>
                     <input type="submit" value="Choisir">
                 </form>
